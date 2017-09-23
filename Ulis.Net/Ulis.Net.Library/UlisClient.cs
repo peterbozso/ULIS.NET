41bbe74ceb0f6ced68932b1e9ee516add479ba88
Péter Bozsó <peter.bozso@outlook.com>
2017-09-23 14:50:22 +0200

    Add call to LUIS

--- Ulis.Net/Ulis.Net.Library/UlisClient.cs
+++ Ulis.Net/Ulis.Net.Library/UlisClient.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Cognitive.LUIS;
 
 namespace Ulis.Net.Library
 {
     public class UlisClient
     {
+        private readonly LuisClient _luisClient;
         private readonly ITranslatorClient _translatorClient;
 
-        public UlisClient(TranslatorClients translatorClient, string subsriptionKey)
+        public UlisClient(TranslatorClients translatorClient, string translatorSubsriptionKey,
+            string luisAppId, string luisAppKey)
         {
             switch (translatorClient)
             {
                 case TranslatorClients.Microsoft:
-                    _translatorClient = new MicrosoftTranslatorClient(subsriptionKey);
+                    _translatorClient = new MicrosoftTranslatorClient(translatorSubsriptionKey);
                     break;
                 case TranslatorClients.Google:
-                    _translatorClient = new GoogleTranslatorClient(subsriptionKey);
+                    _translatorClient = new GoogleTranslatorClient(translatorSubsriptionKey);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(translatorClient));
             }
+
+            _luisClient = new LuisClient(luisAppId, luisAppKey);
         }
 
-        public async Task<string> Translate(string text)
+        public async Task<LuisResult> Query(string text)
         {
-            return await _translatorClient.Translate(text);
+            var englishText = await _translatorClient.Translate(text);
+            return await _luisClient.Predict(englishText);
         }
     }
 }
