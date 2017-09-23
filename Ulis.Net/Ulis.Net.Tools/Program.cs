f79dc47ccd0de30033b1243fd10bf0b2cb2c0ca7
Péter Bozsó <peter.bozso@outlook.com>
2017-09-23 21:18:53 +0200

    Add basic test of UlisClient in Tools

--- Ulis.Net/Ulis.Net.Tools/Program.cs
+++ Ulis.Net/Ulis.Net.Tools/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Ulis.Net.Library;
 
 namespace Ulis.Net.Tools
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            var translatorClient = Boolean.Parse(config["UseMicrosoftTranslator"])
+                ? new MicrosoftTranslatorClient(config["MicrosoftSubscriptionKey"]) as ITranslatorClient
+                : new GoogleTranslatorClient(config["GoogleSubscriptionKey"]);
+
+            var ulisClient = new UlisClient(translatorClient, config["LuisAppId"], config["LuisAppKey"]);
+
+            var result = await ulisClient.QueryAsync("Szia!");
+            Console.WriteLine(
+                $"Translated text: {result.OriginalQuery} | Top scoring intent: {result.TopScoringIntent.Name}");
         }
     }
 }
